ebc501f0f7acfb3a448d33b663100bc662a4d28c
comnows <watthanandh.l@mail.kmutt.ac.th>
2024-07-02 14:02:46 +0700

    feat(backend): add express setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,15 @@
-console.log("run index");
+import express, { Express } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+type portType = string | number;
+
+const app: Express = express();
+const port: portType = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is listening to port ${port}...`);
+});
