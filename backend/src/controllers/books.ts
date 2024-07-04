a4e5ab7482cd7deef7baa140acfacdd4078aa114
comnows <watthanandh.l@mail.kmutt.ac.th>
2024-07-04 11:44:13 +0700

    feat(routes):set up books route controllers

--- backend/src/controllers/books.ts
+++ backend/src/controllers/books.ts
@@ -1,29 +1,110 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 import { query } from "../db";
 
 const getAllBooks = async (req: Request, res: Response) => {
-  const results = await query("SELECT * FROM book");
+  try {
+    const results = await query("SELECT * FROM book");
 
-  console.log(results);
-  res.status(200).json({
-    data: {},
-  });
+    res.status(StatusCodes.OK).json({
+      data: {
+        books: results.rows,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.BAD_GATEWAY).json({
+      data: {
+        error: "Something went wrong, please try again",
+      },
+    });
+  }
 };
-const getBook = (req: Request, res: Response) => {
-  console.log(req, res);
-  res.send("get book");
+
+const getBook = async (req: Request, res: Response) => {
+  try {
+    const results = await query("SELECT * FROM book WHERE id = $1", [
+      req.params.id,
+    ]);
+
+    res.status(StatusCodes.OK).json({
+      data: {
+        books: results.rows,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.BAD_GATEWAY).json({
+      data: {
+        error: "Something went wrong, please try again",
+      },
+    });
+  }
 };
-const createBook = (req: Request, res: Response) => {
-  console.log(req, res);
-  res.send("create books");
+
+const createBook = async (req: Request, res: Response) => {
+  const { name, type } = req.body;
+  try {
+    const results = await query(
+      "INSERT INTO book(name, type) VALUES($1, $2) RETURNING *",
+      [name, type],
+    );
+
+    console.log(results);
+
+    res.status(StatusCodes.CREATED).json({
+      data: {
+        books: results.rows,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.BAD_GATEWAY).json({
+      data: {
+        error: "Something went wrong, please try again",
+      },
+    });
+  }
 };
-const updateBook = (req: Request, res: Response) => {
-  console.log(req, res);
-  res.send("update books");
+
+const updateBook = async (req: Request, res: Response) => {
+  const { name, type } = req.body;
+  try {
+    const results = await query(
+      "UPDATE book SET name = $1, type = $2 WHERE id = $3 RETURNING *",
+      [name, type, req.params.id],
+    );
+
+    res.status(StatusCodes.OK).json({
+      data: {
+        books: results.rows,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.BAD_GATEWAY).json({
+      data: {
+        error: "Something went wrong, please try again",
+      },
+    });
+  }
 };
-const deleteBook = (req: Request, res: Response) => {
-  console.log(req, res);
-  res.send("delete books");
+
+const deleteBook = async (req: Request, res: Response) => {
+  try {
+    await query("DELETE FROM book WHERE id = $1", [req.params.id]);
+
+    res.status(StatusCodes.OK).json({
+      status: "success",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.BAD_GATEWAY).json({
+      data: {
+        error: "Something went wrong, please try again",
+      },
+    });
+  }
 };
 
 export { getAllBooks, getBook, createBook, updateBook, deleteBook };
