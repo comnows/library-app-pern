a1217c9b4953d8eea46a8fdb4dc173d35d67fa38
comnows <watthanandh.l@mail.kmutt.ac.th>
2024-07-16 17:25:46 +0700

    feat(frontend): add lend list options button

--- frontend/src/components/lend/LendList.tsx
+++ frontend/src/components/lend/LendList.tsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import { fetchLendLists } from "../../api";
 import { useLendListContext } from "../../hooks/UseLendListContext";
 import { formatDate } from "../../utils/DateFormat";
+import { LuFileClock } from "react-icons/lu";
+import { FaRegTrashCan } from "react-icons/fa6";
 
 function LendList() {
   const { lendLists, setLendLists } = useLendListContext();
@@ -31,6 +33,7 @@ function LendList() {
             <th className="py-3 px-5">Lend date</th>
             <th className="py-3 px-5">Due date</th>
             <th className="py-3 px-5">Return date</th>
+            <th className="py-3 px-5">Options</th>
           </tr>
         </thead>
         <tbody className="border">
@@ -49,6 +52,14 @@ function LendList() {
                     {lendList.returned_date
                       ? formatDate(lendList.returned_date)
                       : "-"}
+                  </td>
+                  <td className="py-3 px-5">
+                    <button className="group bg-white hover:bg-green-500 border border-green-500 rounded p-1">
+                      <LuFileClock className="text-green-500 group-hover:text-white text-xl" />
+                    </button>
+                    <button className="group bg-white hover:bg-red-600 border border-red-600 rounded p-1 ml-2">
+                      <FaRegTrashCan className="text-red-600 group-hover:text-white text-xl" />
+                    </button>
                   </td>
                 </tr>
               );
