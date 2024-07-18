a00e77d2603ad89177c81916f26324fa33a94739
comnows <watthanandh.l@mail.kmutt.ac.th>
2024-07-18 15:05:00 +0700

    feat(frontend): add return book by book id

--- frontend/src/components/lend/AddLendListForm.tsx
+++ frontend/src/components/lend/AddLendListForm.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import FormButton from "../general/form/FormButton";
 import FormInput from "../general/form/FormInput";
-import { addLendList } from "../../api";
-import { LendInfoType } from "../../lib/types";
+import { addLendList, fetchLendListByBookId } from "../../api";
+import { LendInfoType, LendListType } from "../../lib/types";
 import { useLendListContext } from "../../hooks/UseLendListContext";
 
 function AddLendListForm() {
@@ -11,7 +11,7 @@ function AddLendListForm() {
     bookId: null!,
   });
 
-  const { setLendLists } = useLendListContext();
+  const { setLendLists, updateLendList } = useLendListContext();
 
   const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setLendInfo((current: LendInfoType) => ({
@@ -23,7 +23,32 @@ function AddLendListForm() {
   const handleAddClick = async (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) => {
-    event.preventDefault;
+    event.preventDefault();
+
+    if (!lendInfo.bookId) {
+      alert("Please enter book id or/and member id");
+      return;
+    }
+
+    const fetchBookResponse = await fetchLendListByBookId(lendInfo.bookId);
+    const fetchListData: LendListType = fetchBookResponse.data.data.lists;
+
+    if (fetchListData.length > 0) {
+      if (lendInfo.memberId === "") {
+        updateLendList(fetchListData[0].id);
+        return;
+      } else {
+        alert("This book was lent, remove member id to return the book");
+        return;
+      }
+    } else {
+      if (lendInfo.memberId === "") {
+        alert(
+          "This book doesn't have borrower yet, please enter member id to lend the book",
+        );
+        return;
+      }
+    }
 
     try {
       const response = await addLendList(lendInfo);
--- frontend/src/contexts/LendListContext.tsx
+++ frontend/src/contexts/LendListContext.tsx
@@ -1,10 +1,12 @@
 import { createContext, useState } from "react";
 import { ChildrenType } from "../lib/types";
 import { LendListType } from "../lib/types";
+import { updateLendListReturnDate } from "../api";
 
 type LendListContextType = {
   lendLists: LendListType;
   setLendLists: React.Dispatch<React.SetStateAction<LendListType>>;
+  updateLendList: (id: number) => void;
 };
 
 export const LendListContext = createContext<LendListContextType>(null!);
@@ -12,8 +14,29 @@ export const LendListContext = createContext<LendListContextType>(null!);
 function LendListContextProvider({ children }: ChildrenType) {
   const [lendLists, setLendLists] = useState<LendListType>([]);
 
+  const updateLendList = async (id: number) => {
+    try {
+      const response = await updateLendListReturnDate(id);
+
+      const updatedLendLists: LendListType = lendLists.map((lend) =>
+        lend.id === id
+          ? {
+              ...lend,
+              returned_date: response.data.data.lists[0].returned_date,
+            }
+          : lend,
+      );
+
+      setLendLists(updatedLendLists);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <LendListContext.Provider value={{ lendLists, setLendLists }}>
+    <LendListContext.Provider
+      value={{ lendLists, setLendLists, updateLendList }}
+    >
       {children}
     </LendListContext.Provider>
   );
