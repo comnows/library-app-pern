4fd6ff303eb49e4e9ab351ff17a8da4714717d8e
comnows <watthanandh.l@mail.kmutt.ac.th>
2024-08-02 11:31:21 +0700

    feat: add navbar lend page link

--- frontend/src/components/general/navbar/Sidebar.tsx
+++ frontend/src/components/general/navbar/Sidebar.tsx
@@ -1,22 +1,29 @@
 import SidebarLink from "./SidebarLink";
 import SidebarIcon from "./SidebarIcon";
 import { IoPersonOutline, IoBookOutline } from "react-icons/io5";
+import { FaList } from "react-icons/fa6";
 
 function Sidebar() {
   return (
     <div className="fixed bg-white left-0 inset-y-0 pt-16 w-[240px]">
       <div className="flex flex-col p-3">
-        <SidebarLink>
+        <SidebarLink path="/">
           <SidebarIcon>
             <IoBookOutline />
           </SidebarIcon>
           <p>Book List</p>
         </SidebarLink>
-        <SidebarLink>
+        <SidebarLink path="/">
           <SidebarIcon>
             <IoPersonOutline />
           </SidebarIcon>
           <p>Member List</p>
+        </SidebarLink>
+        <SidebarLink path="/lend">
+          <SidebarIcon>
+            <FaList />
+          </SidebarIcon>
+          <p>Lend List</p>
         </SidebarLink>
       </div>
     </div>
--- frontend/src/components/general/navbar/SidebarLink.tsx
+++ frontend/src/components/general/navbar/SidebarLink.tsx
@@ -1,10 +1,18 @@
+import { Link } from "react-router-dom";
 import { ChildrenType } from "../../../lib/types";
 
-function SidebarLink({ children }: ChildrenType) {
+type SidebarLinkProps = ChildrenType & {
+  path: string;
+};
+
+function SidebarLink({ children, path }: SidebarLinkProps) {
   return (
-    <a className="flex items-center gap-6 text-sm w-full h-10 px-5 rounded-lg hover:bg-black/5">
+    <Link
+      to={path}
+      className="flex items-center gap-6 text-sm w-full h-10 px-5 rounded-lg hover:bg-black/5"
+    >
       {children}
-    </a>
+    </Link>
   );
 }
 
